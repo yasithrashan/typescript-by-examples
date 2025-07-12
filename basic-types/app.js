db389735c0defb380688662dffdec7887afcab82
Yasith Rashan <y.rashan22@gmail.com>
2025-07-12 23:26:13 +0530

    feat: add basic types examples

---
+++ basic-types/app.js
@@ -0,0 +1,15 @@
+"use strict";
+let x = 10;
+let y = 20;
+let z = "Hello, TypeScript!";
+console.log(x);
+console.log(y);
+console.log(z);
+let result = undefined;
+console.log(result);
+result = 2;
+result = "TypeScript is great!";
+console.log(result);
+let result2 = undefined;
+result2 = 3;
+console.log(result2);
---
+++ basic-types/app.ts
@@ -0,0 +1,20 @@
+let x:number = 10;
+let y:number = 20;
+
+let z:string = "Hello, TypeScript!";
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+let result = undefined
+console.log(result);
+
+result = 2;
+result = "TypeScript is great!";
+
+console.log(result);
+
+let result2 : number | undefined = undefined;
+result2 = 3;
+console.log(result2)
